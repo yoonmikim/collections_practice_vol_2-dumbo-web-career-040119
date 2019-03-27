d158bac755db5488b1e20184697821523f9a1131
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-27 17:03:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
-# your code goes here
+your code goes here
+def begins_with_r(arr)
+  i = 0
+  while i < arr.length
+    if !((arr[i]).start_with?("r"))
+      return false
+    end
+    i += 1
+  end
+  return true
+end
